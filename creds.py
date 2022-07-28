5851ab38fd90ff3a68ed94ee9c11586b2d669324
Mohanad Elamin <mohanad.elamin@os3.nl>
2022-07-28 12:24:17 +0100

    add creds file

---
+++ creds.py
@@ -0,0 +1,10 @@
+# ais.usvisa-info.com credentials
+username = 'XXXXXXXXX'
+password = 'XXXXXXXXX'
+url_id = 'XXXXXXXXX' # This is the number id in the url (e.g.: https://ais.usvisa-info.com/en-pe/niv/schedule/41231513)
+appointment_url_id = 'XXXXXXXXXX' # This number in the appointment URL (e.g.: /https://ais.usvisa-info.com/en-gb/niv/schedule/12312412/appointment
+country_code = 'XX-XX' #this is the country code for the consulate e.g. en-nl for Netherlands
+
+# Telegram bot token and chat 
+token = 'XXXXXXXXXXXXX'
+chat_id = 'XXXXXXXXXXXXX'
